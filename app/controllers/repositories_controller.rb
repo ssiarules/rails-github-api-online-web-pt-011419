ac47788b1d3403cdc8949c5754c649ab5321f64c
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-04 03:11:26 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,13 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+   resp = Faraday.get("https://api.github.com/user") do |req|
+   req.params['oauth_token'] = session[:token]
+   # don't forget that pesky v param for versioning
+   req.params['v'] = '20160201'
+ end
+ @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+ render 'friends'
   end
 
 end
